d32e8ff74150910167cd054afe6e383c84430198
Manon Gerray <manon.gerray@gmail.com>
2021-01-08 01:07:09 +0100

    :sparkles: Sorting and Reading are readyyyyyy I'm so happyyyyyyy :tada:

--- components/Reading.js
+++ components/Reading.js
@@ -28,17 +28,26 @@ const SlotStyle = styled.div`
 `;
 
 function Slot({ title, past, present, future }) {
+  const IMAGE =
+    title === "passé" ? past[3] : title === "présent" ? present[3] : future[3];
+  const NAME =
+    title === "passé" ? past[0] : title === "présent" ? present[0] : future[0];
+  const NUMBER =
+    title === "passé" ? past[1] : title === "présent" ? present[1] : future[1];
+  const DESCRIPTION =
+    title === "passé" ? past[2] : title === "présent" ? present[2] : future[2];
+
   return (
     <SlotStyle>
       <div className="image">
-        {/* <img src="/eye.PNG" /> */}
+        <img src={IMAGE} />
         <h2>{title}</h2>
       </div>
       <div className="content">
-        {/* {title === "passé" && <h3>{past}</h3>}
-        {title === "présent" && <h3>{present}</h3>}
-        {title === "futur" && <h3>{future}</h3>} */}
-        <p>Description</p>
+        <h3>
+          {NUMBER}. {NAME}
+        </h3>
+        <p>{DESCRIPTION}</p>
       </div>
     </SlotStyle>
   );
@@ -47,8 +56,40 @@ function Slot({ title, past, present, future }) {
 export default function Reading() {
   let router = useRouter();
 
-  let selectedCards = router?.query?.selectedCards;
-  console.log(selectedCards);
+  function noQuotes(string) {
+    let newString = string.split(`"`);
+    return newString;
+  }
+
+  let splitPast = router?.query?.pastPic.split(`""`);
+  let pastPic = splitPast.toString();
+
+  let splitPresent = router?.query?.presentPic.split(`""`);
+  let presentPic = splitPresent.toString();
+
+  let splitFuture = router?.query?.futurePic.split(`""`);
+  let futurePic = splitFuture.toString();
+
+  let pastCard = [
+    noQuotes(router?.query?.pastName),
+    noQuotes(router?.query?.pastNumber),
+    noQuotes(router?.query?.pastTxt),
+    pastPic.replace(/['"]+/g, ""),
+  ];
+
+  let presentCard = [
+    noQuotes(router?.query?.presentName),
+    noQuotes(router?.query?.presentNumber),
+    noQuotes(router?.query?.presentTxt),
+    presentPic.replace(/['"]+/g, ""),
+  ];
+
+  let futureCard = [
+    noQuotes(router?.query?.futureName),
+    noQuotes(router?.query?.futureNumber),
+    noQuotes(router?.query?.futureTxt),
+    futurePic.replace(/['"]+/g, ""),
+  ];
 
   return (
     <ReadStyle>
@@ -56,9 +97,9 @@ export default function Reading() {
         <Slot
           key={titles[i]}
           title={title}
-          // past={pastCard}
-          // present={presentCard}
-          // future={futureCard}
+          past={pastCard}
+          present={presentCard}
+          future={futureCard}
         />
       ))}
       <Link href="/">Retour à la page d'accueil</Link>
--- components/Sort.js
+++ components/Sort.js
@@ -30,23 +30,51 @@ function shuffle(array) {
 }
 
 export default function Sort({ cards }) {
+  // First, I set my state containing the array with the future selected cards &
+  // my shuffled cards
   const [selectedCards, setSelectedCards] = useState([]);
   const [shuffledCards, setShuffledCards] = useState([]);
 
-  // // My cards are shuffled
-  // let mixedCards = shuffle(cards);
-
-  // Setting an empty array, fill it with the clicked cards
+  // This next function stores the new card in the selectedCards state as long as the array's length is not 3 long
 
   const select = (prop) => {
     if (selectedCards.length < 3 && !selectedCards.includes(prop)) {
       setSelectedCards([...selectedCards, prop]);
     }
-    console.log("modified", selectedCards);
   };
 
-  console.log("state", selectedCards);
+  if (selectedCards.length === 3) {
+    // let pastCard = [
+    //   {
+    //     name: selectedCards[0].name,
+    //     number: selectedCards[0].number,
+    //     description: selectedCards[0].past,
+    //     picture: selectedCards[0].picture,
+    //   },
+    // ];
 
+    // let presentCard = [
+    //   {
+    //     name: selectedCards[1].name,
+    //     number: selectedCards[1].number,
+    //     description: selectedCards[1].present,
+    //     picture: selectedCards[1].picture,
+    //   },
+    // ];
+
+    // let futureCard = [
+    //   {
+    //     name: selectedCards[2].name,
+    //     number: selectedCards[2].number,
+    //     description: selectedCards[2].present,
+    //     picture: selectedCards[2].picture,
+    //   },
+    // ];
+
+    console.log("state :", selectedCards);
+  }
+
+  // My useEffect hook will make sure my cards are shuffled first, and not shuffling when my selectedCards state updated
   useEffect(() => {
     let shuffled = shuffle(cards);
     setShuffledCards(shuffled);
@@ -60,16 +88,38 @@ export default function Sort({ cards }) {
           <div key={card._id} onClick={() => select(card)}>
             <img
               src={
-                selectedCards.includes(card)
-                  ? "/backopen.PNG"
-                  : "/backclosed.PNG"
+                selectedCards.includes(card) ? card.picture : "/backclosed.PNG"
               }
             />
           </div>
         ))}
       </CardsContainer>
       {selectedCards.length === 3 && (
-        <Link href={`/tirage/resultat?selectedCards=${selectedCards}`}>
+        <Link
+          href={`/tirage/resultat?pastName=${JSON.stringify(
+            selectedCards[0].name
+          )}&pastNumber=${JSON.stringify(
+            selectedCards[0].number
+          )}&pastTxt=${JSON.stringify(
+            selectedCards[0].past
+          )}&pastPic=${JSON.stringify(
+            selectedCards[0].picture
+          )}&presentName=${JSON.stringify(
+            selectedCards[1].name
+          )}&presentNumber=${JSON.stringify(
+            selectedCards[1].number
+          )}&presentTxt=${JSON.stringify(
+            selectedCards[1].present
+          )}&presentPic=${JSON.stringify(
+            selectedCards[1].picture
+          )}&futureName=${JSON.stringify(
+            selectedCards[2].name
+          )}&futureNumber=${JSON.stringify(
+            selectedCards[2].number
+          )}&futureTxt=${JSON.stringify(
+            selectedCards[2].future
+          )}&futurePic=${JSON.stringify(selectedCards[2].picture)}`}
+        >
           <a>Voir les résultats de votre tirage</a>
         </Link>
       )}
