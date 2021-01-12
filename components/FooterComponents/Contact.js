ed588549182bd530a3275b844a14b015442ba99c
Manon Gerray <manon.gerray@gmail.com>
2021-01-12 16:34:56 +0100

    :pencil: Contact page content

--- components/FooterComponents/Contact.js
+++ components/FooterComponents/Contact.js
@@ -1,25 +1,47 @@
 import styled from "@emotion/styled";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const ContactContainer = styled.div``;
 
 export default function Contact() {
   return (
     <ContactContainer>
-      <h1>Contactez-moi</h1>
+      <h1>Contactez-moi !</h1>
       <div className="contacts">
-        <div className="contact">
-          <h3>Pour me faire un retour sur le site</h3>
-          {/* <FontAwesomeIcon icon={faEnvelope} /> */}
+        <div className="retours">
+          <h3>J'attends vos retours sur le site</h3>
+          <p className="explain">
+            En vue de toujours m'améliorer et de rendre l'Oracle des Yeux
+            toujours plus agréable à utiliser, j'aimerais collecter vos retours.
+            N'hésitez pas à m'écrire à l'adresse mail prévue à cet effet.
+          </p>
+          <p className="please">
+            Vos retours peuvent pointer des éléments négatifs, mais n'oubliez
+            pas de pointer le positif aussi, ça fait toujours plaisir !
+          </p>
+          <h4>feedbacksapp.manon@gmail.com</h4>
         </div>
-        <div className="contact">
-          <h3>Pour parler perspectives d'emploi ?</h3>
-          {/* <FontAwesomeIcon icon={faLinkedin} /> */}
-        </div>
-        <div className="contact">
-          <h3>Venez jeter un oeil au code du site !</h3>
-          {/* <FontAwesomeIcon icon={faGithub} /> */}
+        <div className="pro">
+          <h3>Envie de discuter professionnellement ?</h3>
+          <p className="explain">
+            Vous êtes développeur ou futur développeur et souhaitez discuter ?
+            Ou vous êtes peut-être un recruteur ou CTO à la recherche de son
+            prochain talent et mon profil vous intéresse ? Voici mes différents
+            réseaux et contacts.
+          </p>
+          <div className="infos">
+            <Link href="https://www.linkedin.com/in/manongerray/">
+              <a>Ma page Linkedin</a>
+            </Link>
+            <Link href="https://www.github.com/manonxrray">
+              <a>Ma page Github</a>
+            </Link>
+            <Link href="https://twitter.com/mnoudev">
+              <a>Mon compte Twitter</a>
+            </Link>
+            <p className="plus">et par mail à l'adresse suivante :</p>
+            <h4>manon.gerray@gmail.com</h4>
+          </div>
         </div>
       </div>
     </ContactContainer>
